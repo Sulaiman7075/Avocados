4d6b02dc6789751cee374927f6f284423a470fd8
Sulaiman7075 <sulaimanhassanb@gmail.com>
2025-10-22 21:31:06 +0500

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Sulaiman on 22/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
